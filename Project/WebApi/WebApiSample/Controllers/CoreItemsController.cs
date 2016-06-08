dff83c0332e672405b17c23564fb6d194a295607
dhavalcric <dhaval.cric@gmail.com>
2016-06-08 09:57:31 -0400

    Sample projects

---
+++ Project/WebApi/WebApiSample/Controllers/CoreItemsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace WebApplication1.Controllers
+{
+    public class CoreItemsController : ApiController
+    {
+        CoreEntities coreItemData;
+        public CoreItemsController() {
+            coreItemData = new CoreEntities();
+        }
+        //public HttpResponseMessage Get(string itemNumber)
+        //{
+        //    CoreItem cItem;
+        //    cItem = coreItemData.CoreItems.Where(x => x.ItemNumber == itemNumber).FirstOrDefault();
+        //    if(cItem!=null)
+        //        return Request.CreateResponse(HttpStatusCode.OK, cItem);
+        //    else
+        //        return Request.CreateResponse(HttpStatusCode.NotFound, "Item not found in system with Item Number:" + itemNumber);
+        //}
+
+        public IHttpActionResult Get(string itemNumber)
+        {
+            CoreItem cItem;
+            cItem = coreItemData.CoreItems.Where(x => x.ItemNumber == itemNumber).FirstOrDefault();
+            if (cItem != null)
+                return Ok(cItem);
+            else
+                return NotFound();
+        }
+
+        public IEnumerable<CoreItem> Get()
+        {
+
+            return coreItemData.CoreItems.Where(x=> x.CopyrightYear == "2016").ToList(); 
+        }
+
+        
+
+        public CoreItem Put(string itemNumber, CoreItem coreItemToUpdate)
+        {
+            CoreItem coreItem =  coreItemData.CoreItems.Where(x => x.ItemNumber == itemNumber).FirstOrDefault();
+
+            coreItem.ItemDescription = coreItemToUpdate.ItemDescription;
+            coreItem.Price = coreItemToUpdate.Price;
+
+            coreItemData.SaveChanges();
+           
+
+            return coreItemToUpdate;
+        }
+    }
+}
